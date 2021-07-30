f0acec30c32479512cd616a03dd21c2db711b030
angraRaccoon <victor.reis@raccoon.ag>
2021-07-30 14:09:04 -0300

    refactor(master): add gulp files and browser sync into projeto02

--- projeto02/gulpfile.js
+++ projeto02/gulpfile.js
@@ -1,6 +1,54 @@
-function defaultTask(cb) {
-    // place code for your default task here
-    cb();
-  }
-  
-  exports.default = defaultTask
+const { src, dest, watch, series } = require('gulp')
+const sass = require('gulp-sass')(require('sass'))
+const postcss = require('gulp-postcss')
+const cssnano = require('cssnano')
+const terser = require('gulp-terser')
+const browsersync = require('browser-sync').create()
+
+
+//sass Task
+function sassUpdate(){
+  return src('styles/index.sass', { sourcemaps: true })
+    .pipe(sass())
+    .pipe(postcss([cssnano()]))
+    .pipe(dest('styles', { sourcemaps: '.' }));
+}
+
+//javscript Task
+function jsTask(){
+  return src('scripts/index.js', { sourcemaps: true })
+    .pipe(terser())
+    .pipe(dest('scripts',{ sourcemaps: '.' }))
+}
+
+//Browsersync
+function browsersyncServe(cb){
+  browsersync.init({
+    server: {
+      baseDir: '.'
+    }
+  })
+  cb()
+}
+
+//Browsersync reload
+function browsersyncReload(cb){
+  browsersync.reload()
+  cb()
+}
+
+// Watch Task
+
+function watchTask(){
+  watch('*.html', browsersyncReload)
+  watch(['styles/index.sass','scripts/index.js'], series(sassUpdate,jsTask, browsersyncReload))
+}
+
+//Default gulptask
+exports.default = series(
+  sassUpdate,
+  jsTask,
+  browsersyncServe,
+  browsersyncReload,
+  watchTask
+)
---
+++ projeto02/scripts/index.js
@@ -0,0 +1,2 @@
+const coll=document.getElementsByClassName("collapsible");for(let l=0;l<coll.length;l++)coll[l].addEventListener("click",(()=>{"active"==coll[l].classList[1]?coll[l].classList.remove("active"):coll[l].classList.add("active");let e=coll[l].nextElementSibling;e.style.maxHeight?e.style.maxHeight=null:e.style.maxHeight=e.scrollHeight+"px"}));
+//# sourceMappingURL=index.js.map
