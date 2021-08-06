98d9a6741277ce6827d06314fc320a179028d22a
angraRaccoon <victor.reis@raccoon.ag>
2021-08-06 11:12:24 -0300

    feat(master): add mobile caroussel sixth section into projeto02

---
+++ projeto02/scripts/carousel.js
@@ -0,0 +1,79 @@
+
+// Linkando os slides do carousel e os botões
+const track = document.querySelector('.carousel__track')
+const slides = Array.from(track.children)
+const nextButton = document.querySelector('.carousel__button--right') 
+const prevButton = document.querySelector('.carousel__button--left')
+
+// Linkando os dots do nav
+const nav = document.querySelector('.carousel__nav')
+const dots = Array.from(nav.children)
+
+const slideWidth = slides[0].getBoundingClientRect().width
+
+
+slides.forEach((slide, index) => {
+    slide.style.left = `${slideWidth * index}px`
+})
+
+const moveToSlide = (track, currentSlide, targetSlide) => {
+
+    track.style.transform = `translateX(-${targetSlide.style.left})`
+    currentSlide.classList.remove('current-slide')
+    targetSlide.classList.add('current-slide')
+    const h1 = targetSlide.children[0]
+    const h5 = targetSlide.children[1]
+    const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
+    if(vw >= 1024){
+        h1.style.marginTop = '10%'
+        h5.style.marginTop = '25%'
+        console.log('maior que 1024px')
+    }else{
+        console.log('menor que 1024px')
+    }
+
+}
+
+const updateDots = (currentDot, targetDot) => {
+    currentDot.classList.remove('current-slide')
+    targetDot.classList.add('current-slide')
+}
+
+// Evento ao clicar botão esquerdo
+prevButton.addEventListener('click', (ev) => {
+    const currentSlide = track.querySelector('.current-slide') 
+    const prevSlide = currentSlide.previousElementSibling
+    const currentDot = nav.querySelector('.current-slide')
+    const prevDot = currentDot.previousElementSibling
+
+    moveToSlide(track, currentSlide, prevSlide)
+    updateDots(currentDot, prevDot)
+})
+
+// Evento ao clicar o botão direito
+nextButton.addEventListener('click', (ev) => {
+
+    const currentSlide = track.querySelector('.current-slide') 
+    const nextSlide = currentSlide.nextElementSibling
+    const currentDot = nav.querySelector('.current-slide')
+    const nextDot = currentDot.nextElementSibling
+
+    moveToSlide(track, currentSlide, nextSlide)
+    updateDots(currentDot, nextDot)
+})
+
+// Clicar nos dots
+nav.addEventListener('click', (ev) => {
+
+    const targetDot = ev.target.closest('button')
+   
+    if(!targetDot) return
+    
+    const currentSlide = track.querySelector('.current-slide')
+    const currentDot = nav.querySelector('.current-slide')
+    const targetIndex = dots.findIndex(dot => dot === targetDot)
+    const targetSlide = slides[targetIndex]
+
+    moveToSlide(track, currentSlide, targetSlide)
+    updateDots(currentDot, targetDot)
+})
---
+++ projeto02/scripts/competence-container.js
@@ -0,0 +1,38 @@
+
+// Mobile Competence Container
+const mobileCompetenceContainerFunction = () => {
+
+    const coll=document.getElementsByClassName("collapsible");
+    for(let l=0;l<coll.length;l++)
+        coll[l].addEventListener("click",(()=>{
+            "active"==coll[l].classList[1]?coll[l].classList.remove("active"):coll[l].classList.add("active");
+            let e=coll[l].nextElementSibling;
+            e.style.maxHeight?e.style.maxHeight=null:e.style.maxHeight=e.scrollHeight+"px"
+        }));
+
+}
+
+// Desktop Competence Container
+const desktopCompetenceContainerFunction = () => {
+
+    const competence_container = document.getElementsByClassName('desktop-competence-container')[0]
+    const button_container = competence_container.firstElementChild
+    const competence_buttons = button_container.children
+
+    for(let i = 0; i < competence_buttons.length;i++){
+        competence_buttons[i].addEventListener("click", () => {
+            competence_buttons[i].classList.add("active")
+            for(let j = 0; j < competence_buttons.length; j++){
+                if(i != j && competence_buttons[j].classList[0] == "active"){
+                    competence_buttons[j].classList.remove("active")
+                }
+            }
+        })
+    }
+
+
+}
+
+// Rodando
+mobileCompetenceContainerFunction()
+desktopCompetenceContainerFunction()
